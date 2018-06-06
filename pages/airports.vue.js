831a8f10656cdf8118792b809591e425fd246c02
Steven Olensky <steve@olis.com>
2018-06-06 17:19:18 -0500

    Started with FB

--- pages/airports.vue.js
+++ pages/airports.vue.js
@@ -1,73 +1,65 @@
 var spaAirports = Vue.component("Airports", {
-  template: `<div>
+	template: `<div>
   <h3>Airport Delays</h3>
   <v-card class="mb-2">
-    <v-card-media height="400" src="https://dsx.weather.com/util/image/map/airport_delays_1280x720.jpg?v=ap&w=1280&h=720&api=7db9fe61-7414-47b5-9871-e17d87b8b6a0">
+	<v-card-media height="400" 
+		src="https://dsx.weather.com/util/image/map/airport_delays_1280x720.jpg?v=ap&w=1280&h=720&api=7db9fe61-7414-47b5-9871-e17d87b8b6a0">
     </v-card-media>
     <v-card-text>
       <v-layout row>
-        <v-flex xs12>
-          <v-select ref="apts" :items="airportsUS" v-on:input="Lookup" v-model="airportCode" autocomplete item-text="longname" item-value="iata"
-           clearable label="Airports">
+        <v-flex xs10>
+		  <v-select :items="airportsUS"  v-model="airports"
+		   multiple
+           label="Airports">
           </v-select>
         </v-flex>
       </v-layout>
     </v-card-text>
   </v-card>
-  <card-airport :airport="airportData"></card-airport>
+<div v-for="airport in airportsSorted" :key="airport">
+	  <card-airport :airportCode="airport"></card-airport>
+	  <v-divider></v-divider>
+  </div>
 </div>`,
-  props: ["title"],
-  $_veeValidate: {
-    validator: "new"
-  },
-  computed: {
-    airportsUS: function() {
-      var items = this.airportList.filter(function(item) {
-        item.longname = item.iata + " - " + item.name;
-        return item.iso == "US" && item.size == "large";
-      });
-      return items;
-    }
-  },
-  data: function() {
-    return {
-      airportCode: "",
-      airportData: {
-        name: null,
-        status: {
-          reason: ""
-        }
-      },
-      airportList: iata
-    };
-  },
-  methods: {
-    Lookup: function() {
-      let self = this;
-      let url =
-        "https://services.faa.gov/airport/status/" +
-        this.airportCode +
-        "?format=application/json";
-      axios
-        .get(url)
-        .then(function(response) {
-          self.airportData = response.data;
-          //console.log(response);
-          self.loading = false;
-        })
-        .catch(function(error) {
-          self.airportData = {
-            name: null,
-            status: {
-              reason: ""
-            }
-          };
-          console.log("error", error);
-        });
-    },
-    ClearList() {
-      console.log(this.airportCode, this.airportData);
-      this.$refs.apts.value = "";
-    }
-  }
+	props: ["title"],
+	$_veeValidate: {
+		validator: "new"
+	},
+	computed: {
+		airportsUS: function () {
+			var items = []
+			this.airportList.forEach(item => {
+				if (item.iso == "US" && item.size == "large") {
+					items.push(item.iata)
+				}
+			});
+			return items;
+		},
+		airportsSorted() {
+			var as = _.sortBy(this.airports, [function (o) {
+				return o;
+			}]);
+			return as
+		}
+	},
+	data: function () {
+		return {
+			airportCode: "",
+			airports: [],
+			airportList: iata
+		};
+	},
+	methods: {
+		Lookup() {
+			if (this.airportCode) {
+				this.airports.push({
+					id: this.airportCode
+				})
+			}
+		},
+		ClearList() {
+			console.log(this.airportCode, this.airportData);
+			this.$refs.apts.value = "";
+		}
+	}
 });
--- pages/components/airportcard.vue.js
+++ pages/components/airportcard.vue.js
@@ -1,24 +1,69 @@
 Vue.component("card-airport", {
-  template: `
-<div v-if="airport.name!=null">
+    template: `
+<div>
     <v-card dark v-bind:color="this.airport.delay==='true' ? 'error' : 'success' ">
         <v-card-text>
-            <h4>{{airport.IATA}} {{airport.name}} {{airport.city}} {{airport.state}} {{airport.weather.meta.updated}}</h4>
+            <h4>{{airport.IATA}} {{airport.name}} {{airport.city}} {{airport.state}} 
+                {{airport.weather.meta.updated}}</h4>
             <v-spacer></v-spacer>
             <h5>
-                {{airport.status.reason}} {{airport.status.minDelay}} {{airport.delay!='true' ? '' : ' to '}} {{airport.status.maxDelay}}
+                {{airport.status.reason}} {{airport.status.minDelay}} 
+                {{airport.delay!='true' ? '' : ' to '}} {{airport.status.maxDelay}}
                 {{airport.status.type}} {{airport.status.trend}}
             </h5>
             <h5>
-                temp: {{airport.weather.temp}}, visibility: {{airport.weather.visibility}}, weather: {{airport.weather.weather}}, wind: {{airport.weather.wind}}
+                temp: {{airport.weather.temp}}, visibility: {{airport.weather.visibility}}, 
+                weather: {{airport.weather.weather}}, wind: {{airport.weather.wind}}
             </h5>
         </v-card-text>
     </v-card>
 </div>
   `,
-  props: ["airport"],
-  $_veeValidate: {
-    validator: "new"
-  },
-  methods: {}
+    props: ["airportCode"],
+    $_veeValidate: {
+        validator: "new"
+    },
+    data: function () {
+        return {
+            airport: {
+                status: {},
+                weather: {
+                    meta: {}
+                }
+            }
+        }
+    },
+    mounted() {
+        this.Lookup()
+        //console.log('created', this.airportCode)
+    },
+    methods: {
+        Lookup: function () {
+            //console.log('lookup', this.airportCode)
+            let url =
+                "https://services.faa.gov/airport/status/" +
+                this.airportCode +
+                "?format=application/json";
+            axios
+                .get(url)
+                .then(response => {
+                    this.airport = response.data;
+                    this.airport.xdate = moment().format();
+                    //console.log(this.airport);
+                    //db.collection('sch').add(response.data).then(ss => {
+                    //console.log('fbResult', ss.id)
+                    //})
+                    this.loading = false;
+                })
+                .catch(error => {
+                    this.airport = {
+                        name: null,
+                        status: {
+                            reason: ""
+                        }
+                    };
+                    console.log("error", error);
+                });
+        },
+    }
 });
